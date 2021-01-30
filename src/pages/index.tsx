4d587c55f50b427835f95c0855d843c8c6bbded4
Gediminas Milašius <gediminasmilasius@gmail.com>
2021-01-30 16:41:00 +0200

    Static - Nothing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,9 @@
-import { GetServerSideProps } from 'next';
 import { useMemo } from 'react';
-import * as fs from 'fs';
 
-type Props = { jobs: typeof import('data/jobs.json')['data']['jobs'] };
-
-export default function Home({ jobs }: Props) {
+export default function Home() {
   const dataString = useMemo(() => {
-    return JSON.stringify(jobs);
-  }, [jobs]);
+    return JSON.stringify({});
+  }, []);
 
   return dataString;
 }
-
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  const df: typeof import('data/jobs.json') = JSON.parse(
-    fs.readFileSync('data/jobs.json', 'utf-8')
-  );
-
-  return { props: { jobs: df.data.jobs } };
-};
