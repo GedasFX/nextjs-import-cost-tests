2a47d7950e73d728ef9f48aaf6adce48b8212bd5
Gediminas Milašius <gediminasmilasius@gmail.com>
2021-01-30 17:09:51 +0200

    Static - Client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import { useMemo } from 'react';
+import data from 'data/jobs.json';
 
 export default function Home() {
   const dataString = useMemo(() => {
-    return JSON.stringify({});
+    return JSON.stringify(data.data.jobs);
   }, []);
 
   return dataString;
