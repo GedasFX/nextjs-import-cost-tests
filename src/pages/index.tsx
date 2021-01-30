6d1ac1d4ceea3246ae5b81a1f9266d2d55635990
Gediminas Milašius <gediminasmilasius@gmail.com>
2021-01-30 14:19:26 +0200

    static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
+import { GetStaticProps } from 'next';
+import { useMemo } from 'react';
+import df from 'data/i.json';
+
 type Props = { data: typeof import('data/i.json') };
 
-export default function Home() {
+export default function Home({ data }: Props) {
+  const dataString = useMemo(() => {
+    return JSON.stringify(data);
+  }, [data]);
+
   return (
     <>
       <span>Hello World!</span>
+      {dataString}
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  return { props: { data: df } };
+};
