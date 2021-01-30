dcfd3d537aa0d9949ac6fc6b74dee890c9035c07
Gediminas Milašius <gediminasmilasius@gmail.com>
2021-01-30 18:25:31 +0200

    SSG - Apollo (Server)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from 'react';
-import * as fs from 'fs';
-import type { GetServerSideProps } from 'next';
+import { ApolloClient, createHttpLink, InMemoryCache, gql } from '@apollo/client';
+import type { GetStaticProps } from 'next';
 
 type Props = {
   jobs: typeof import('data/jobs.json')['data']['jobs'];
@@ -14,9 +14,34 @@ export default function Home({ jobs }: Props) {
   return dataString;
 }
 
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  const data: typeof import('data/jobs.json') = JSON.parse(
-    fs.readFileSync('data/jobs.json', 'utf-8')
-  );
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const client = new ApolloClient({
+    ssrMode: true,
+    link: createHttpLink({
+      uri: 'https://api.graphql.jobs/',
+    }),
+    cache: new InMemoryCache(),
+  });
+  const data = await client.query<typeof import('data/jobs.json')['data']>({
+    query: gql`
+      {
+        jobs {
+          title
+          description
+          commitment {
+            title
+          }
+          cities {
+            name
+            country {
+              isoCode
+              name
+            }
+          }
+        }
+      }
+    `,
+  });
+
   return { props: { jobs: data.data.jobs } };
 };
