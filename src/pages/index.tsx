4869c78342083683fda71d6f97ce6e7f270076dd
Gediminas Milašius <gediminasmilasius@gmail.com>
2021-01-30 18:44:26 +0200

    SSG - Apollo (Server + Client)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,54 @@
-import { useMemo } from 'react';
-import { ApolloClient, createHttpLink, InMemoryCache, gql } from '@apollo/client';
-import type { GetServerSideProps } from 'next';
+import { useEffect, useMemo } from 'react';
+import { ApolloClient, createHttpLink, InMemoryCache, gql, ApolloProvider } from '@apollo/client';
+import type { GetStaticProps } from 'next';
 
 type Props = {
   jobs: typeof import('data/jobs.json')['data']['jobs'];
 };
+
+const client1 = new ApolloClient({
+  link: createHttpLink({
+    uri: 'https://api.graphql.jobs/',
+  }),
+  cache: new InMemoryCache(),
+});
 
 export default function Home({ jobs }: Props) {
   const dataString = useMemo(() => {
     return JSON.stringify(jobs);
   }, [jobs]);
 
-  return dataString;
+  useEffect(() => {
+    client1
+      .query<typeof import('data/jobs.json')['data']>({
+        query: gql`
+          {
+            jobs {
+              title
+              description
+              commitment {
+                title
+              }
+              cities {
+                name
+                country {
+                  isoCode
+                  name
+                }
+              }
+            }
+          }
+        `,
+      })
+      .then(data => {
+        console.log(data);
+      });
+  }, []);
+
+  return <ApolloProvider client={client1}>{dataString}</ApolloProvider>;
 }
 
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
+export const getStaticProps: GetStaticProps<Props> = async () => {
   const client = new ApolloClient({
     ssrMode: true,
     link: createHttpLink({
