8e1e2838e87f9dac83595a7acb0f3a681f32d0db
Gediminas Milašius <gediminasmilasius@gmail.com>
2021-01-30 17:43:46 +0200

    SSG - Server + Client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from 'react';
 import data from 'data/jobs.json';
-import type { GetServerSideProps } from 'next';
+import type { GetStaticProps } from 'next';
 
 type Props = {
   jobs: typeof import('data/jobs.json')['data']['jobs'];
@@ -8,12 +8,14 @@ type Props = {
 
 export default function Home({ jobs }: Props) {
   const dataString = useMemo(() => {
-    return JSON.stringify(jobs);
+    if (JSON.stringify(data.data.jobs) === JSON.stringify(jobs)) {
+      return JSON.stringify(jobs);
+    }
   }, [jobs]);
 
   return dataString;
 }
 
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
+export const getStaticProps: GetStaticProps<Props> = async () => {
   return { props: { jobs: data.data.jobs } };
 };
